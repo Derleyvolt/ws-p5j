0322b4b4d71cc505f67cb44e3f76d231304d02c0
derleyvolt <100Palavr@gmail.com>
2023-08-16 00:59:09 -0300

    some message

--- src/BinaryAuxiliary/binaryHandler.js
+++
@@ -1,5 +0,0 @@
-// Assumindo que 
-
-function writeInt32(value, index, array) {
-    array[index] = 0xFF & value;
-}
--- src/HandlerMessage/SerializeMessage.js
+++ src/HandlerMessage/SerializeMessage.js
@@ -1,14 +1,20 @@
+const bitwise = require('../bitwiseUtils/binaryHandler');
+
 function stringToASCII(string) {
     return string.split('').map(e => e.charCodeAt(0));
 }
 
+function stringToASCIIWithNullTerminated(string) {
+    let result = stringToASCII(string);
+    return [...result, 0];
+}
+
 function entityDetails(entity) {
-    const name       = stringToASCII(entity.getName());
-    const nameLength = name.length;
+    const name       = stringToASCIIWithNullTerminated(entity.getName());
     const velocity   = entity.getVelocity(); // 1 byte
     const position   = entity.getPosition(); // 4 bytes
-    const radius     = entity.getRadius(); // 1 byte
-    const health     = entity.getHealth(); // 2 bytes
+    const radius     = entity.getRadius();   // 1 byte
+    const health     = entity.getHealth();   // 2 bytes
 
-    const buffer = new Uint8Array
+    const buffer = new Uint8Array[name.length + 8];
 }
